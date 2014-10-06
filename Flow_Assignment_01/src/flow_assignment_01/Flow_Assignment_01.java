36521021de9c79744fa1aa80c155d393b2a73031
Armandi <Jonas_hjp@hotmail.com>
2014-10-06 10:38:40 +0200

    Start
    v_02

---
+++ Flow_Assignment_01/src/flow_assignment_01/Flow_Assignment_01.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package flow_assignment_01;
+
+/**
+ *
+ * @author Armandi
+ */
+public class Flow_Assignment_01 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
